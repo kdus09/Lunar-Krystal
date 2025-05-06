bba971ffbb7f5311c189c886b851d5272c6ed10e
kdus09 <lethekhoi209@hotmail.com>
2025-05-06 21:15:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Define a route
 app.get('/', (request, response) => {
-    const result = `Nhớ ib Facebook Lương Trường Khôi để cập nhật file nha (free) Facebook: https://facebook.com/Khoi.Meta`;
+    const result = `Nhớ ib Facebook Admin để cập nhật file nha (free) Facebook: https://www.facebook.com/coder09`;
     response.send(result);
 });
 // Start the server
